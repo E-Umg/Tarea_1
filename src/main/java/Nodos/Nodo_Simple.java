50c11684f99c998a8a1246da73d4e149c6785d7a
Mizago <alvaroal5115@gmail.com>
2022-09-02 23:41:33 -0600

    Add two activiys and desing the connection beetween

---
+++ src/main/java/Nodos/Nodo_Simple.java
@@ -0,0 +1,16 @@
+package Nodos;
+
+import java.util.Scanner;
+
+public class Nodo_Simple {
+    public static void main(String[] args) {
+
+            Scanner tecla = new Scanner(Syste.in);
+            String a = "";
+
+            System.out.println("Cliente:");
+            a = tecla.nextLine();
+
+
+    }
+}
