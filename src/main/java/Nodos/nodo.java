36d93974373a1eeb2529b5c0dc03edc3127af4d3
Mizago <alvaroal5115@gmail.com>
2022-09-06 17:59:42 -0600

    Rectificaion #2

--- src/main/java/Nodos/nodo.java
+++ src/main/java/Nodos/nodo.java
@@ -3,7 +3,15 @@ package Nodos;
 public class nodo {
     public static void main(String[] args) {
         System.out.printf("hello word");
+        String a = "a";
 
+        if (a == a){
+            System.out.println("Prueva asegurada");
+        }else{
+            System.out.println("java presenta inconvenietes");
+
+
+        }
     }
 
 
